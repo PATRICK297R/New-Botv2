f111cd9b3d75f3c6f111ebb65098ea7aebba1c4e
Patrick <96850073+PATRICK297R@users.noreply.github.com>
2024-08-22 11:00:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,36 +1,82 @@
-global.owner = ['62895331520602']  
-global.mods = ['62895331520602'] 
-global.prems = ['62895331520602']
-global.nameowner = 'Tio'
-global.numberowner = '62895331520602' 
-global.mail = 'support@tioprm.eu.org' 
-global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
-global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
-global.wait = '_*Tunggu sedang di proses...*_'
-global.eror = '_*Server Error*_'
-global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
-global.packname = 'Made With'
-global.author = 'Bot WhatsApp'
-global.autobio = false // Set true untuk mengaktifkan autobio
-global.maxwarn = '2' // Peringatan maksimum
+// Thanks For Allah
+// AlyaaXzy
+// BOTCAHX
 
-//INI WAJIB DI ISI!//
-global.btc = 'YOUR_APIKEY_HERE' 
-//Daftar terlebih dahulu https://api.botcahx.eu.org
-
-//INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
-global.lann = 'YOUR_APIKEY_HERE'
-//Daftar https://api.betabotz.eu.org 
-
-global.APIs = {   
-  btc: 'https://api.botcahx.eu.org'
-}
-global.APIKeys = { 
-  'https://api.botcahx.eu.org': 'APIKEY' 
-}
 
 let fs = require('fs')
+global.owner = ['6285735980188', '6285815212637'] // Letakan nomor kamu disini
+global.APIs = { // API Prefix
+  // nama: 'https://website'
+  hardianto: 'https://hardianto.xyz',
+  rey: 'https://server-api-rey.herokuapp.com',
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zekais: 'http://zekais-api.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  vhtear: 'https://api.vhtear.com',
+  lolhum: 'https://api.lolhuman.xyz',
+  fxc7: 'https://fxc7-api.herokuapp.com',
+  bx: 'https://bx-hunter.herokuapp.com',
+
+}
+global.APIKeys = { // APIKey nya disini
+  // 'https://website': 'apikey'
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://hardianto.xyz': 'hardianto',
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://pencarikode.xyz': 'APIKEY',
+  'https://api.xteam.xyz': 'MIMINETBOT',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'k16uXLA0glrWpzuz4bK9v3HNA1l',
+  'https://api.lolhuman.xyz': 'patricksus',
+  'https://api.vhtear.com': 'sayahafiz',
+  'https://fxc7-api.herokuapp.com': 'uN8rsK4g',
+  'https://api.justaqul.xyz': '5kbUqdG00OXelFYx',
+  'http://zekais-api.herokuapp.com': 'zekais',
+  'https://bx-hunter.herokuapp.com': 'Ikyy69',
+}
+
+// Sticker WM
+const spack = fs.readFileSync("lib/exif.json")
+const stickerpack = JSON.parse(spack)
+if (stickerpack.spackname == '') {
+  var sticker_name = 'Made By'
+  var sticker_author = 'PatrickBotz'
+} else {
+  var sticker_name = stickerpack.spackname
+  var sticker_author = stickerpack.sauthor
+}
+
+const file_exif = "lib/exif.json"
+fs.watchFile(file_exif, () => {
+  fs.unwatchFile(file_exif)
+  console.log(chalk.redBright("Update 'exif.json'"))
+  delete require.cache[file_exif]
+  require('./lib/exif.json')
+})
+
+global.packname = sticker_name
+global.author = sticker_author
+
+global.wait = '_*Loading...*_'
+global.eror = 'Terjadi Kesalahan'
+global.fla = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text='
+global.watermark = '© PatrickBotz' //change the watermark 
+global.image = 'https://telegra.ph/file/cd97946f4c6e1c9277848.jpg' //change the image
+global.thumbfoto = 'https://telegra.ph/file/cd97946f4c6e1c9277848.jpg'
+
+// image
+global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
+global.kandang = 'https://telegra.ph/file/67a6ee607d03a4e52757d.jpg'
+global.kolam = 'https://telegra.ph/file/5aa5dfa3394477e11fb18.jpg'
+global.thanks = 'https://telegra.ph/file/01917f7782b70de8f418d.jpg'
+
+global.multiplier = 100 // Semakin tinggi, semakin sulit naik level
+
 let chalk = require('chalk')
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
